81f1cc9dab05ac3fa5b009448dc152efc4ac36f9
jrheat2005 <jrheat2005@gmail.com>
2023-09-26 12:33:18 -0700

    added projects

---
+++ Basic_C#_Programs/Math Input/Math Input/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Math_Input
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // take input and mutiply by 50
+            Console.Write("Enter a number: ");
+            decimal num1 = decimal.Parse(Console.ReadLine());
+            num1 = num1 * 50;
+            Console.WriteLine(num1);
+
+            // take input and add 25 
+            Console.Write("Enter a number: ");
+            int num2 = int.Parse(Console.ReadLine());
+            num2 += 25;
+            Console.WriteLine(num2);
+
+            // take input and divide by 12.5
+            Console.Write("Enter a number: ");
+            double num3 = double.Parse(Console.ReadLine());
+            num3 /= 12.5;
+            Console.WriteLine(num3);
+
+            // take input check if greater than 50, than print true or false accordingly 
+            Console.Write("Enter a number: ");
+            int num4 = int.Parse(Console.ReadLine());
+            bool isGreater = num4 > 50;
+            Console.WriteLine(isGreater);
+
+            // take input and divide by 7 and print the remainder
+            Console.Write("Enter a number: ");
+            int num5 = int.Parse(Console.ReadLine());
+            int remainder = num5 % 7;
+            Console.WriteLine(remainder);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Basic_C#_Programs/Operations/Operations/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Operations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(5 + 5);
+            Console.WriteLine(5 - 5);
+            Console.WriteLine(5 * 5);
+            Console.WriteLine(5 / 5);
+
+
+        }
+    }
+}
