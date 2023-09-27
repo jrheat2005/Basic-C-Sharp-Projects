39da91106849bdf8d47854e5ee7c0e8f2502f629
jrheat2005 <jrheat2005@gmail.com>
2023-09-27 10:30:18 -0700

    added more projects

---
+++ Basic_C#_Programs/BooleanLogic/BooleanLogic/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace BooleanLogic
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine(true && true);
+            //Console.WriteLine(false && false);
+
+            Console.WriteLine(true || false);
+            Console.WriteLine(true || true);
+            Console.WriteLine(false || false);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Basic_C#_Programs/BooleanLogicAssignment/BooleanLogicAssignment/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BooleanLogicAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // asking and storing all the data 
+            Console.WriteLine("Please answser the questions below");
+            Console.WriteLine("What is your age?");
+            int age = int.Parse(Console.ReadLine());
+            Console.WriteLine("Have you ever had a DUI? (Please anwser in true or false)");
+            bool dui = bool.Parse(Console.ReadLine());
+            Console.WriteLine("How many speeding tickets do you have?");
+            int speeding = int.Parse(Console.ReadLine());
+
+            // check if over 15
+            bool overAge = 15 < age;
+
+            // check if under 3 tickets
+            bool speedingNumber = 3 >= speeding;
+
+            // checking if it passes all the qualifcations
+            bool qualified = overAge && speedingNumber ^ dui;
+
+            // display results
+            Console.WriteLine("What is your age?\n" + age);
+            Console.WriteLine("Have you ever had a DUI?\n" + dui);
+            Console.WriteLine("How many speeding tickets do you have?\n" + speeding);
+            Console.WriteLine("Qualified?\n" + qualified);
+
+            // wait before closing
+            Console.ReadLine();
+        }
+    }
+}
