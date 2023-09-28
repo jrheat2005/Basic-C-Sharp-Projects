934d955cdba5b251384634eee869906c93d57403
jrheat2005 <jrheat2005@gmail.com>
2023-09-28 11:47:06 -0700

    added more projects

---
+++ Basic_C#_Programs/IfElse/IfElse/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IfElse
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = "John";
+            if (name == "John")
+            {
+                Console.WriteLine("Correct");
+            }
+            else
+            {
+                Console.WriteLine("Wrong");
+            }
+
+            if (name != "John")
+            {
+                Console.WriteLine("Correct");
+            }
+            else
+            {
+                Console.WriteLine("Wrong");
+            }
+
+            int age = 14;
+            if (age <= 21)
+            {
+                Console.WriteLine("Old enough");
+            }
+            else
+            {
+                Console.WriteLine("Not old enough");
+            }
+            if (age > 21)
+            {
+                Console.WriteLine("Old enough");
+            }
+            else
+            {
+                Console.WriteLine("Not old enough");
+            }
+
+            int num1 = 17;
+            int num2 = 19;
+
+            string result = num1 > num2 ? "num1 is greater than num2" : "num1 is not greater than num2";
+            Console.WriteLine(result);
+            Console.ReadLine();
+        }
+    }
+}
