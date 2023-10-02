38cd99b4b09b8196719c03c6642497d04ed42905
jrheat2005 <jrheat2005@gmail.com>
2023-10-02 15:11:31 -0700

    added projects

---
+++ Basic_C#_Programs/Method Submission/Method Submission/MathOperations.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Method_Submission
+{
+    public class MathOperations
+    {
+        // Method 1: Add 10 to the input
+        public int AddTen(int number)
+        {
+            return number + 10;
+        }
+
+        // Method 2: Subtract 5 from the input
+        public int SubtractFive(int number)
+        {
+            return number - 5;
+        }
+
+        // Method 3: Multiply the input by 2
+        public int MultiplyByTwo(int number)
+        {
+            return number * 2;
+        }
+    }
+    
+}
---
+++ Basic_C#_Programs/Method Submission/Method Submission/Program.cs
@@ -0,0 +1,45 @@
+﻿using Method_Submission;
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        // Ask the user for a number
+        Console.Write("Enter a number for math operations: ");
+
+        try
+        {
+            // Read the user input as a string
+            string userInput = Console.ReadLine();
+
+            // Convert the user input to an integer
+            int number = int.Parse(userInput);
+
+            // Create an instance of the MathOperations class
+            MathOperations mathOps = new MathOperations();
+
+            // Call each method and display the results
+            int result1 = mathOps.AddTen(number);
+            int result2 = mathOps.SubtractFive(number);
+            int result3 = mathOps.MultiplyByTwo(number);
+
+            Console.WriteLine($"Result of adding 10: {result1}");
+            Console.WriteLine($"Result of subtracting 5: {result2}");
+            Console.WriteLine($"Result of multiplying by 2: {result3}");
+        }
+        catch (FormatException)
+        {
+            // Handle the case where the user enters non-integer input
+            Console.WriteLine("Error: Please enter a valid integer.");
+        }
+        catch (Exception ex)
+        {
+            // Handle any other unexpected exceptions
+            Console.WriteLine($"An error occurred: {ex.Message}");
+        }
+
+        Console.WriteLine("Press any key to exit...");
+        Console.ReadKey();
+    }
+}
