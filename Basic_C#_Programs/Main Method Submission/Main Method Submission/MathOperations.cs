f6c10ca57b99da7e4d0a7d5d688ba4c8eb898d6e
jrheat2005 <jrheat2005@gmail.com>
2023-10-02 14:33:59 -0700

    added project

---
+++ Basic_C#_Programs/Main Method Submission/Main Method Submission/MathOperations.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+public class MathOperations
+{
+    // Method 1: Add 10 to the input number
+    public static int AddTen(int number)
+    {
+        return number + 10;
+    }
+
+    // Method 2: Subtract 5 from the input number
+    public static int SubtractFive(int number)
+    {
+        return number - 5;
+    }
+
+    // Method 3: Multiply the input number by 2
+    public static int MultiplyByTwo(int number)
+    {
+        return number * 2;
+    }
+}
---
+++ Basic_C#_Programs/Main Method Submission/Main Method Submission/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        // Ask the user for a number to perform math operations on
+        Console.Write("Enter a number: ");
+
+        try
+        {
+            // Read the user input as a string
+            string userInput = Console.ReadLine();
+
+            // Convert the user input to an integer
+            int userNumber = int.Parse(userInput);
+
+            // Call each method from the MathOperations class and display the results
+            int result1 = MathOperations.AddTen(userNumber);
+            Console.WriteLine($"Adding 10 to {userNumber} gives: {result1}");
+
+            int result2 = MathOperations.SubtractFive(userNumber);
+            Console.WriteLine($"Subtracting 5 from {userNumber} gives: {result2}");
+
+            int result3 = MathOperations.MultiplyByTwo(userNumber);
+            Console.WriteLine($"Multiplying {userNumber} by 2 gives: {result3}");
+        }
+        catch (FormatException)
+        {
+            // Handle the case where the user enters a non-integer string
+            Console.WriteLine("Error: Please enter a valid integer.");
+        }
+        catch (Exception ex)
+        {
+            // Handle any other unexpected exceptions
+            Console.WriteLine($"An error occurred: {ex.Message}");
+        }
+
+        // This line will be executed after the try/catch block
+        Console.WriteLine("Program finished.");
+        Console.ReadLine();
+    }
+}
