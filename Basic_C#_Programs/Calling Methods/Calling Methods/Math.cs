d399da355ac03cb062c641afadb77371a59255fb
jrheat2005 <jrheat2005@gmail.com>
2023-10-02 12:31:00 -0700

    added projects

---
+++ Basic_C#_Programs/Calling Methods/Calling Methods/Math.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calling_Methods
+{
+    public class Math
+    {
+        // Method to double the input integer
+        public int Double(int num)
+        {
+            return num * 2;
+        }
+
+        // Method to square the input integer
+        public int Square(int num)
+        {
+            return num * num;
+        }
+
+        // Method to calculate the factorial of the input integer
+        public int Factorial(int num)
+        {
+            if (num == 0)
+            {
+                return 1;
+            }
+            int result = 1;
+            for (int i = 1; i <= num; i++)
+            {
+                result *= i;
+            }
+            return result;
+        }
+    }
--- Basic_C#_Programs/Calling Methods/Calling Methods/Program.cs
+++ Basic_C#_Programs/Calling Methods/Calling Methods/Program.cs
@@ -10,6 +10,27 @@ namespace Calling_Methods
     {
         static void Main(string[] args)
         {
+            // Create an instance of the Math class
+            Math math = new Math();
+
+            // Ask the user for a integer input
+            Console.Write("Enter an integer: ");
+            if (int.TryParse(Console.ReadLine(), out int UserInput))
+            {
+                // Call each method and display the results
+                int doubled = math.Double(UserInput);
+                int squared = math.Square(UserInput);
+                int factorial = math.Factorial(UserInput);
+
+                Console.WriteLine($"Double: {doubled}");
+                Console.WriteLine($"Square: {squared}");
+                Console.WriteLine($"Factorial: {factorial}");
+            }
+            else
+            {
+                Console.WriteLine("Invalid input. Please enter a valid integer.");
+            }
+            Console.ReadLine();
         }
     }
 }
