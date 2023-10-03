2b029abfeca774fdb718016a153651e4ef12a7c0
jrheat2005 <jrheat2005@gmail.com>
2023-10-03 11:44:24 -0700

    fixed?

--- Basic_C#_Programs/Method Submission/Method Submission/MathOperations.cs
+++ Basic_C#_Programs/Method Submission/Method Submission/MathOperations.cs
@@ -1,19 +1,9 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace Method_Submission
+public class MathCalculator
 {
-    public class MathCalculator
+    public int PerformMathOperation(int num1, int num2 = 0)
     {
-        // Created a method that takes two integers as parameters, with one of them optional (default value is 0).
-        public int PerformMathOperation(int number1, int number2 = 0)
-        {
-            // Add the numbers together
-            int result = number1 + number2;
-            return result;
-        }
+        return num1 + num2;
     }
 }
--- Basic_C#_Programs/Method Submission/Method Submission/Program.cs
+++ Basic_C#_Programs/Method Submission/Method Submission/Program.cs
@@ -1,37 +1,50 @@
-﻿using Method_Submission;
-using System;
+﻿using System;
 
 class Program
 {
-    static void Main()
+    static void Main(string[] args)
     {
-        // Instantiate the MathCalculator class
-        MathCalculator calculator = new MathCalculator();
+        MathCalculator calc = new MathCalculator();
 
-        // Ask the user to input two numbers, one at a time
-        Console.Write("Enter the first number: ");
-        int input1 = int.Parse(Console.ReadLine());
+        // Get input 1
+        Console.Write("Enter first number: ");
+        int input1;
 
-        Console.Write("Enter the second number (press Enter to skip): ");
-        string input2Str = Console.ReadLine();
-
-        // Initialize input2 with a default value of 0
-        int input2 = 0;
-
-        // Check if the user entered a second number
-        if (!string.IsNullOrEmpty(input2Str))
+        if (int.TryParse(Console.ReadLine(), out input1))
         {
-            // Parse the second input as an integer
-            input2 = int.Parse(input2Str);
+            // Get input 2
+            Console.Write("Enter second number (optional): ");
+            string input2Str = Console.ReadLine();
+
+            int input2;
+
+            if (string.IsNullOrWhiteSpace(input2Str))
+            {
+                // If input2 is empty or whitespace, use the default value (0)
+                int result = calc.PerformMathOperation(input1); // Call with one parameter
+                Console.WriteLine($"Result: {result}");
+            }
+            else if (int.TryParse(input2Str, out input2))
+            {
+                // Input is valid, call with both parameters
+                int result = calc.PerformMathOperation(input1, input2);
+                Console.WriteLine($"Result: {result}");
+            }
+            else
+            {
+                // Input is invalid, but we'll use the default value (0)
+                Console.WriteLine("Invalid input for the second number. Using the default value (0).");
+                int result = calc.PerformMathOperation(input1); // Call with one parameter
+                Console.WriteLine($"Result: {result}");
+            }
+        }
+        else
+        {
+            // Input is invalid
+            Console.WriteLine("Invalid input for the first number.");
         }
 
-        // Call the PerformMathOperation method with the provided inputs
-        int result = calculator.PerformMathOperation(input1, input2);
-
-        // Display the result
-        Console.WriteLine($"Result of the math operation: {result}");
-
-        // Wait for user input before exiting (optional)
+        // Wait for key press
         Console.WriteLine("Press any key to exit...");
         Console.ReadKey();
     }
