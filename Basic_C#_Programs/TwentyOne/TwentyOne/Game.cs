3a5343427fae749c141f1013f35c5255f7ad370a
jrheat2005 <jrheat2005@gmail.com>
2023-10-03 14:03:42 -0700

    update

---
+++ Basic_C#_Programs/TwentyOne/TwentyOne/Game.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TwentyOne
+{
+	public class Game
+	{
+        public List<string> Players { get; set; }
+        public string Name { get; set; }
+        public string Dealer { get; set; }
+        public void ListPlayer()
+        {
+            foreach (string player in Players)
+            {
+                Console.WriteLine(player);
+            }
+        }
+    }
+}
--- Basic_C#_Programs/TwentyOne/TwentyOne/Program.cs
+++ Basic_C#_Programs/TwentyOne/TwentyOne/Program.cs
@@ -10,6 +10,9 @@ namespace TwentyOne
     {
         static void Main(string[] args)
         {
+            Game game = new Game();
+            game.Dealer = "Jesse";
+            game.Name = "TwentyOne";
             Deck deck = new Deck();
             deck.Shuffle(3);
 
@@ -21,14 +24,5 @@ namespace TwentyOne
                 
             Console.ReadLine();
         }
-        
-        //public static Deck Shuffle(Deck deck, int times)
-        //{
-        //    for (int i = 0; i < times; i++)
-        //    {
-        //        deck = Shuffle(deck);
-        //    }
-        //    return deck;
-        //}
     }
 }
