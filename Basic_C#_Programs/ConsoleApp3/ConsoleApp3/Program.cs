55d49f976c29b455e8c7cc7a9af8cf65752ea4ec
jrheat2005 <jrheat2005@gmail.com>
2023-10-14 14:40:23 -0700

    added projects

---
+++ Basic_C#_Programs/ConsoleApp3/ConsoleApp3/Program.cs
@@ -0,0 +1,20 @@
+﻿using ConsoleApp3;
+using System.Diagnostics;
+namespace ConsoleApp3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            using (var ctx = new SchoolContext())
+            {
+                var stud = new Student() { StudentName = "Bill" };
+
+                ctx.Students.Add(stud);
+                ctx.SaveChanges();
+            }
+        }
+    }
+}
+
